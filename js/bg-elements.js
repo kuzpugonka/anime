b4487c59bc8c64ede3a445051a8f8acdb227a446
Софья Илясова <116354773+kuzpugonka@users.noreply.github.com>
2024-03-01 21:29:40 +0300

    render top5 rating

--- js/bg-elements.js
+++ js/bg-elements.js
@@ -1,10 +1,9 @@
 const bgElements = () => {
   const elements = document.querySelectorAll(".set-bg");
 
-  for (let i = 0; i < elements.length; i++) {
-    const src = elements[i].dataset.setbg;
+  elements.forEach((elem) => {
+    elem.style.backgroundImage = `url(${elem.dataset.setbg})`;
+  });
 
-    elements[i].style.backgroundImage = `url(${src})`;
-  }
 };
 bgElements();
--- js/mainData.js
+++ js/mainData.js
@@ -1,11 +1,37 @@
 const mainData = () => {
-  fetch('../db.json')
+  const renderTopAnime = (array) => {
+    const wrapper = document.querySelector(".filter__gallery");
+    wrapper.innerHTML = "";
+
+    array.forEach((item) => {
+      // console.log(item);
+      wrapper.insertAdjacentHTML(
+        "afterbegin",
+        `
+        <div class="product__sidebar__view__item set-bg mix"
+          data-setbg="${item.image}">
+          <div class="ep">${item.rating} / 10</div>
+          <div class="view"><i class="fa fa-eye"></i> ${item.views}</div>
+          <h5><a href="/anime-details.html" >${item.title}</a></h5>
+        </div>
+      `
+      );
+    });
+
+    const elements = wrapper.querySelectorAll(".set-bg");
+    elements.forEach((elem) => {
+      elem.style.backgroundImage = `url(${elem.dataset.setbg})`;
+    });
+    
+  };
+
+  fetch("../db.json")
     .then((response) => {
-    return response.json()
-  })
-  .then((data) => {
-    console.log(data.anime);
-  })
+      return response.json();
+    })
+    .then((data) => {
+      renderTopAnime(data.anime.sort((a, b) => b.views - a.views).slice(0, 5));
+    });
 };
 
 mainData();
