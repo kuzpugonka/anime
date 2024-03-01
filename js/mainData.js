26e58eb092a226b0db01bb7c418f2f75b79aad7e
Софья Илясова <116354773+kuzpugonka@users.noreply.github.com>
2024-03-01 17:02:43 +0300

    firebase bd-json

--- js/mainData.js
+++ js/mainData.js
@@ -1,10 +1,8 @@
 const mainData = () => {
-  fetch('../db.json')
-    .then((response) => {
-    return response.json()
-  })
-  .then((data) => {
-    console.log(data.anime);
+  fetch('https://anime-bd-7d6a1-default-rtdb.firebaseio.com/anime.json')
+    .then(res => res.json())
+    .then(data => {
+      console.log(data.anime);
   })
 };
 
