f7e72e3da809697d40077c0f9788077887308085
Софья Илясова <116354773+kuzpugonka@users.noreply.github.com>
2024-03-02 15:31:54 +0300

    Update mainData.js
     изменила путь до файла базы данных внутрь проекта

--- js/mainData.js
+++ js/mainData.js
@@ -1,5 +1,5 @@
 const mainData = () => {
-  fetch('https://anime-bd-7d6a1-default-rtdb.firebaseio.com/anime.json')
+  fetch('./db.json')
     .then(res => res.json())
     .then(data => {
       console.log(data.anime);
